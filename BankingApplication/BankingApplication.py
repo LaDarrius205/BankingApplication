806e67abd17146c03d8aa794723dfb14e05b47f5
Ladarrius Caldwell <LCaldwell1@student.fullsail.edu>
2023-04-19 10:01:00 -0400

    Added the registration screen fixed minor bugs

--- BankingApplication/BankingApplication.py
+++ BankingApplication/BankingApplication.py
@@ -3,71 +3,93 @@ import os
 from PIL import ImageTk, Image
 
 
-# Main Screen
+### Main Screen
 master = Tk()
 master.title('Banking App')
 
 
-def finishregister():
-    # Grabbing Details
+def finishRegister():
+    ## Grabbing Details
     name = temp_name.get()
     age = temp_age.get()
     email = temp_email.get()
     password = temp_password.get()
     
-    # Filing System
+    ## Filing System
     all_accounts = os.listdir()
     print(all_accounts)
 
+    # To secure registeration data from previous users
+    if name == "" or age == "" or email == "" or  password == "":
+        notif.config(fg="red",text="***All fields required***")
+        return
+    print("Registeration Successful")
+
+
+    for name_check in all_accounts:
+        if name == name_check:
+            notif.config(fg="red", text="***Account already exists***")
+            return
+        else:
+            new_file = open(name,"w")
+            new_file.write(name+'\n')
+            new_file.write(password+'\n')
+            new_file.write(age+'\n')
+            new_file.write(email+'\n')
+            new_file.close()
+            notif.config(fg='green', text="Account has been created")
        
-#Functions
-def Register():
-    #Variables
+### Functions
+def register():
+    ## Variables
     global temp_name
     global temp_age
     global temp_email
     global temp_password
+    global notif
 
     temp_name = StringVar()
     temp_age = StringVar()
     temp_email = StringVar()
     temp_password = StringVar()
-    #print("This is the Register screen")
+    print("This is the Register screen")
 
-    ###Register Screen... 
+    ###Register Screen... Error not reading
     #Creates a little window once the button is pressed
-    registerScreen = Toplevel(master)
-    registerScreen.title('Register')
+    register_Screen = Toplevel(master)
+    register_Screen.title('Register')
 
 
 #Labels
-    Label(registerScreen, 
+    Label(register_Screen, 
           text= 'Please enter your details below to register', 
       font=('Calibri', 12)).grid(row=0, sticky=N, pady=10)
-    Label(registerScreen, text= 'Name', 
+    Label(register_Screen, text= 'Name', 
       font=('Calibri', 12)).grid(row=1, sticky=W)
-    Label(registerScreen, text= 'Age', 
+    Label(register_Screen, text= 'Age', 
       font=('Calibri', 12)).grid(row=2, sticky=W)
-    Label(registerScreen, text= 'Email', 
+    Label(register_Screen, text= 'Email', 
       font=('Calibri', 12)).grid(row=3, sticky=W,)
-    Label(registerScreen, text= 'Password', 
+    Label(register_Screen, text= 'Password', 
       font=('Calibri', 12)).grid(row=4, sticky=W)
+    notif = Label(register_Screen, font=('Calibri', 12))
+    notif.grid(row=5, sticky=W)
 
     #Entries
-    Entry(registerScreen, 
+    Entry(register_Screen, 
           textvariable=temp_name).grid(row=1, column=0)
-    Entry(registerScreen, 
+    Entry(register_Screen, 
           textvariable=temp_age).grid(row=2, column=0)
-    Entry(registerScreen, 
+    Entry(register_Screen, 
           textvariable=temp_email).grid(row=3, column=0)
-    Entry(registerScreen, textvariable=temp_password, 
+    Entry(register_Screen, textvariable=temp_password, 
           show = '*').grid(row=4, column=0)
 
 
     #Button error <newline> Unexpected Token
-    #Button(registerScreen, text="Register", command = finishRegister, 
-    #       font = ("Calibri", 12).grid(row = 5, sticky = N, pady = 10)
-
+    Button(register_Screen, text="Register", command = finishRegister, 
+           font = ("Calibri", 12)).grid(row = 5, sticky = N, pady = 10)
+    
 
 def login():
      print('this is the login page')
@@ -107,11 +129,11 @@ Label(master, image=img).grid(row=2, sticky=N, pady=15)
 Button(master, text='Register', 
        font=('Calibri', 12),
        width =20 , 
-       command = 'Register').grid(row=3,sticky=N)
-Button(master, text='Login', 
+       command = register).grid(row=3,sticky=N)
+Button(master, text='login', 
        font=('Calibri', 12),
        width =20,
-       command = 'Login').grid(row=4,sticky=N)
+       command = login).grid(row=4,sticky=N)
 
 
 
